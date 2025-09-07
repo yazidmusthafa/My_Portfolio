aeae042a180433922a8544a095ff866f960b6f8c
YAZID MUSTHAFA <yazidmusthafa>
2025-09-07 13:23:40 +0400

    Fixed

---
+++ _next/static/chunks/d0447323-66eed8a4973ae50b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3415],{97735:function(t,h,n){n.d(h,{ndt:function(){return e}});var a=n(88357);function e(t){return(0,a.w_)({tag:"svg",attr:{role:"img",viewBox:"0 0 24 24"},child:[{tag:"title",attr:{},child:[]},{tag:"path",attr:{d:"M1.019 13.019h3.849V24h-3.85zm8.943-6.68h3.85V24h-3.85zM19.132 0h3.85v24h-3.85z"}}]})(t)}}}]);
